7f96555f29bfd16dfe4e9b15a41bfe5ecb1e3358
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-10-11 10:14:33 -0700

    Update Hashlock.py

--- Hashlock.py
+++ Hashlock.py
@@ -13,7 +13,7 @@ def main():
             
         @sp.entrypoint
         def commit(self, receiver, amount, hash):
-            assert sp.sender == self.data.sender, "Not Admin"
+            assert sp.source == self.data.sender, "Not Admin"
             assert self.data.revealed == True, "Old Hash Not Revealed"
             assert sp.amount >= amount, "Not enough tez."
             self.data.revealed = False
@@ -27,60 +27,61 @@ def main():
 
         @sp.entrypoint
         def reveal(self, secretKey):
+            sp.cast(secretKey, sp.nat)
             assert sp.now >= self.data.revealTime, "5 mins need to pass"
-            assert self.data.revealed == False, "Commit has been revealed"
+            assert self.data.revealed == False, "Already revealed"
             bytes_secret = sp.pack(secretKey)
             hashedNumber = sp.sha256(bytes_secret)
             salted = hashedNumber + sp.pack(sp.source)
             revealHash = sp.sha256(salted)
-            assert self.data.saltedHash == revealHash, "Did not match"
+            assert self.data.saltedHash == revealHash, "Didn't match"
             self.data.revealed = True
             sp.send(self.data.receiver, self.data.amount)
                 
 @sp.add_test(name = "Hashlock")
 def test():
-        alice = sp.test_account("Sender")
-        bob = sp.test_account("Receiver")
-        mallory = sp.test_account("Attacker")
+    alice = sp.test_account("Sender")
+    bob = sp.test_account("Receiver")
+    mallory = sp.test_account("Attacker")
     
-        r = main.Hashlock(alice.address)
-        s = sp.test_scenario(main)
-        s.h1("Contract Origination")
-        s += r
+    r = main.Hashlock(alice.address)
+    s = sp.test_scenario(main)
+    s.h1("Contract Origination")
+    s += r
 
-        secret_key = sp.nat(92018)
-        bytes_secret_key = sp.pack(secret_key)
-        hashInput = sp.sha256(bytes_secret_key)
-        wrong_key = sp.nat(102018)
-        amount = sp.tez(2)
-        wrong_amount = sp.tez(1)
+    secret_key = sp.nat(92018)
+    bytes_secret_key = sp.pack(secret_key)
+    hashInput = sp.sha256(bytes_secret_key)
+    wrong_key = sp.nat(102018)
+    amount = sp.tez(2)
+    wrong_amount = sp.tez(1)
 
-        s.h3("Admin doesn't have enough tez to commit")
-        r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(sender=alice.address, amount=wrong_amount, now=sp.timestamp_from_utc_now(), valid=False)
+    s.h3("Admin doesn't have enough tez to commit")
+    r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=alice.address, amount=wrong_amount, now=sp.timestamp_from_utc_now(), valid=False)
 
-        s.h3("Admin successfully commits")
-        r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=alice.address, amount=amount, now=sp.timestamp_from_utc_now())
+    s.h3("Admin successfully commits")
+    r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=alice.address, amount=amount, now=sp.timestamp_from_utc_now())
 
-        s.h3("Attacker calls commit")
-        r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(sender=mallory.address, amount=amount, now=sp.timestamp_from_utc_now(), valid=False)
+    s.h3("Attacker calls commit")
+    r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=mallory.address, amount=amount, now=sp.timestamp_from_utc_now(), valid=False)
 
-        s.h3("Admin calls commit twice")
-        r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(sender=admin.address, amount=amount, now=sp.timestamp_from_utc_now(), valid=False)
+    s.h3("Admin calls commit twice")
+    r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=alice.address, amount=amount, now=sp.timestamp_from_utc_now(), valid=False)
 
-        s.h3("Receiver cannot reveal unless 5 minutes have passed")
-        r.reveal(secret_key).run(sender=bob.address, now=sp.timestamp_from_utc_now(), valid=False)
+    s.h3("Receiver cannot reveal unless 5 minutes have passed")
+    r.reveal(secret_key).run(source=bob.address, now=sp.timestamp_from_utc_now(), valid=False)
 
-        s.h3("Receiver calls reveal with the wrong secret key")
-        r.reveal(wrong_key).run(source=bob.address, now=sp.timestamp_from_utc_now().add_seconds(400), valid=False)
+    s.h3("Receiver calls reveal with the wrong secret key")
+    r.reveal(wrong_key).run(source=bob.address, now=sp.timestamp_from_utc_now().add_seconds(400), valid=False)
 
-        s.h3("Attacker calls reveal")
-        r.reveal(secret_key).run(source=mallory.address, now=sp.timestamp_from_utc_now().add_seconds(400), valid=False)
+    s.h3("Attacker calls reveal")
+    r.reveal(secret_key).run(source=mallory.address, now=sp.timestamp_from_utc_now().add_seconds(400), valid=False)
 
-        s.h3("Receiver successfully calls reveal")
-        r.reveal(secret_key).run(source=bob.address, now=sp.timestamp_from_utc_now().add_seconds(400))
+    s.h3("Receiver successfully calls reveal")
+    r.reveal(secret_key).run(source=bob.address, now=sp.timestamp_from_utc_now().add_seconds(400))
 
-        s.h3("Receiver calls reveal twice")
-        r.reveal(secret_key).run(sender=bob.address, now=sp.timestamp_from_utc_now().add_seconds(600), valid=False)
+    s.h3("Receiver calls reveal twice")
+    r.reveal(secret_key).run(source=bob.address, now=sp.timestamp_from_utc_now().add_seconds(600), valid=False)
         
-        s.h3("Admin successfully commits again")
-        r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=alice.address, amount=amount, now=sp.timestamp_from_utc_now())
+    s.h3("Admin successfully commits again")
+    r.commit(receiver=bob.address, amount=amount, hash=hashInput).run(source=alice.address, amount=amount, now=sp.timestamp_from_utc_now())
